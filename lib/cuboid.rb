f625e2a5b4b1d5b2237a6db57d496f7b964c2143
theberbie <ahbabousara@gmail.com>
2018-08-16 01:10:40 -0700

    initial commit

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,17 +1,73 @@
 
 class Cuboid
-  
-  #BEGIN public methods that should be your starting point
+
+  def initialize(origin, length, width, height)
+    @origin = origin
+    @length = length
+    @width = width
+    @height = height
+
+    if length < 0 || width < 0 || height < 0
+      raise "Please enter positive number"
+    end
+  end
 
   def move_to!(x, y, z)
-  end
-  
-  def vertices
-  end
-  
-  #returns true if the two cuboids intersect each other.  False otherwise.
-  def intersects?(other)
+    @origin[:x] = x
+    @origin[:y] = y
+    @origin[:z] = z
+    return @origin
   end
 
-  #END public methods that should be your starting point  
+  def vertices
+    @vertices_array = []
+    @vertices_array << [min_x, min_y, min_z]
+    @vertices_array << [max_x, min_y, min_z]
+    @vertices_array << [min_x, max_y, min_z]
+    @vertices_array << [min_x, min_y, max_z]
+    @vertices_array << [max_x, max_y, min_z]
+    @vertices_array << [min_x, max_y, max_z]
+    @vertices_array << [max_x, min_y, max_z]
+    @vertices_array << [max_x, max_y, max_z]
+    return @vertices_array
+  end
+
+  #returns true if the two cuboids intersect each other.  False otherwise.
+  def intersects?(other)
+   return false if self.max_x < other.min_x
+   return false if self.min_x > other.max_x
+   return false if self.max_y < other.min_y
+   return false if self.min_y > other.max_y
+   return false if self.max_z < other.min_z
+   return false if self.min_z > other.max_z
+   true
+  end
+
+  protected
+
+  def min_x
+    @origin[:x]
+  end
+
+  def max_x
+    @origin[:x] + @length
+  end
+
+  def min_y
+    @origin[:y]
+  end
+
+  def max_y
+    @origin[:y] + @height
+  end
+
+  def min_z
+    @origin[:z]
+  end
+
+  def max_z
+    @origin[:z] + @width
+  end
+
+  #END public methods that should be your starting point
 end
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -1,17 +1,54 @@
 require 'cuboid'
 
-#This test is incomplete and, in fact, won't even run without errors.  
+#This test is incomplete and, in fact, won't even run without errors.
 #  Do whatever you need to do to make it work and please add your own test cases for as many
 #  methods as you feel need coverage
 describe Cuboid do
- 
-  describe "move_to" do
-    it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
+  subject(:cuboid) { Cuboid.new({x:1, y: 2,z: 3}, 4,5,6) }
+  subject(:neg_cuboid) { Cuboid.new({x:-1, y:-1, z:-1}, -1, 1, -1) }
+
+  describe "initialize" do
+    it 'returns an error if input is less than 0'do
+      expect{ neg_cuboid }.to raise_error
     end
-  end    
-  
-  describe "intersects?" do
   end
 
+  describe "move_to" do
+    it "changes the origin in the simple happy case" do
+      expect(cuboid.move_to!(1,2,3)).to eq({x:1, y:2, z:3})
+    end
+  end
+
+  describe "intersects?" do
+    it "returns false when the cuboids don't intersect" do
+      current = Cuboid.new({x:1, y:1, z:1}, 2,2,2)
+      other = Cuboid.new({x:4, y:4, z:4},2,4,4)
+      expect(current.intersects?(other)).to be false
+    end
+
+    it "returns true when the cuboids intersect" do
+      current = Cuboid.new({x:1, y:1, z:1}, 1,1,1)
+      other = Cuboid.new({x:1, y:1, z:1}, 1, 1,1)
+      expect(current.intersects?(other)).to be true
+      end
+  end
+
+  describe "vertices" do
+    it "returns all the verticies" do
+      expect(cuboid.vertices).to eq([
+        [1,2,3],
+        [5,2,3],
+        [1,8,3],
+        [1,2,8],
+        [5,8,3],
+        [1,8,8],
+        [5,2,8],
+        [5,8,8]
+       ])
+    end
+
+    it "returns 8 vertices" do
+      expect(cuboid.vertices.length).to eq(8)
+    end
+  end
 end
